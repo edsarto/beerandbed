74cc177424cbd0343eb2853aeff0e02ad7ce536f
Edouard Sartorius <edouardsartorius@gmail.com>
2015-12-10 16:54:07 +0100

    Now points mechanics okay except for the booking cancellation from client.

--- app/models/merit/point_rules.rb
+++ app/models/merit/point_rules.rb
@@ -13,25 +13,10 @@ module Merit
     include Merit::PointRulesMethods
 
     def initialize
-      score 20, on: 'account/bookings/reviews#create', model_name: 'reviews'
-      score 20, on: 'account/bookings/accept#create', model_name: 'accept'
+      score 10, on: 'account/bookings/reviews#create', model_name: 'reviews'
+      score 10, on: 'account/bookings/accept#create', model_name: 'accept'
 
-      score -20, on: 'account/bookings/reject#create', model_name: 'reject' do |booking|
-        current_user == @booking.bed.owner?
-      end
-
-      # score 10, :on => 'users#create' do |user|
-      #   user.bio.present?
-      # end
-      #
-      # score 15, :on => 'reviews#create', :to => [:reviewer, :reviewed]
-      #
-      # score 20, :on => [
-      #   'comments#create',
-      #   'photos#create'
-      # ]
-      #
-      # score -10, :on => 'comments#destroy'
+      score -10, on: 'account/bookings/reject#create', model_name: 'reject'
     end
   end
 end
